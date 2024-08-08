17b6b6e13c657bfdb90fc33add975fbc84363a74
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-08-08 21:22:00 +0500

    Create main.cpp

---
+++ 01 - Introductory Problems/02 - Input and Output/main.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    
+    cin>> a >> b >> c;
+    
+    int total = a+b+c;
+    cout<<total;
+    
+    
+    return 0;
+}
