2ecb8328192306cb3968f82f9a75762f6a07274f
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-08-11 20:08:22 +0500

    Create main.cpp

---
+++ 02 - Strings Problems/02 - Strings/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin>> a >> b;
+    
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<(a+b)<<endl;
+    cout<<b.substr(0, 1)<<a.substr(1, a.size())<<" "<<a.substr(0, 1)<<b.substr(1, b.size());
+    
+    return 0;
+}
